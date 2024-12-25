5ca9bea2c524e9a323c21804d001ce24e4ef06c9
SangGeon <yangsk0526@naver.com>
2024-12-25 17:41:40 +0900

    삭제

--- src/main/java/com/knock/shop/ItemController.java
+++ src/main/java/com/knock/shop/ItemController.java
@@ -1,14 +1,11 @@
 package com.knock.shop;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -62,5 +59,20 @@ public class ItemController {
     }
 
 
+    }
+
+    @PostMapping("/test")
+    String test(@RequestBody Map<String, Object> body) {
+        System.out.println(body.get("title"));
+        return "redirect:/list";
+    }
+
+    @DeleteMapping("/deleteItem")
+    ResponseEntity<String> deleteItem(@RequestParam Long id) {
+        System.out.println(id);
+        itemRepository.deleteById(id);
+
+        // 리다이렉트 확인
+        return ResponseEntity.status(200).body("삭제완료"); // list 페이지로 이동
     }
 }
